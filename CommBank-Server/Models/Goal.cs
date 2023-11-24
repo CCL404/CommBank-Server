89c8c2e4fd4d3f2caf9c80fdff58baf332ba83e2
cclois <cclois@hotmail.com>
2023-11-24 16:18:01 +1100

    Back to original Goal.cs and Goal.json

--- CommBank-Server/Models/Goal.cs
+++ CommBank-Server/Models/Goal.cs
@@ -1,35 +1,30 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-using System;
-using System.Collections.Generic;
 
-namespace CommBank.Models
+namespace CommBank.Models;
+
+public class Goal
 {
-    public class Goal
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string? Id { get; set; }
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
 
-        public string? Name { get; set; }
+    public string? Name { get; set; }
 
-        public UInt64 TargetAmount { get; set; } = 0;
+    public UInt64 TargetAmount { get; set; } = 0;
 
-        public DateTime TargetDate { get; set; }
+    public DateTime TargetDate { get; set; }
 
-        public double Balance { get; set; } = 0.00;
+    public double Balance { get; set; } = 0.00;
 
-        public DateTime Created { get; set; } = DateTime.Now;
+    public DateTime Created { get; set; } = DateTime.Now;
 
-        [BsonRepresentation(BsonType.ObjectId)]
-        public List<string>? TransactionIds { get; set; }
+    [BsonRepresentation(BsonType.ObjectId)]
+    public List<string>? TransactionIds { get; set; }
 
-        [BsonRepresentation(BsonType.ObjectId)]
-        public List<string>? TagIds { get; set; }
+    [BsonRepresentation(BsonType.ObjectId)]
+    public List<string>? TagIds { get; set; }
 
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string? UserId { get; set; }
-
-        public string? Icon { get; set; }
-    }
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? UserId { get; set; }
 }
--- CommBank-Server/obj/Debug/net6.0/CommBank.AssemblyInfo.cs
+++ CommBank-Server/obj/Debug/net6.0/CommBank.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommBank-Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58ebf59d36f41a10c1568a574c46ef0a29803ea9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommBank-Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommBank-Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
