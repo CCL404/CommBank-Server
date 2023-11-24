543027e9dcf3aca8e0f17b8c5fdb4235c87d9077
cclois <cclois@hotmail.com>
2023-11-24 21:25:00 +1100

    Cover the `GetGoalsForUser` route

--- CommBank-Server/obj/Debug/net6.0/CommBank.AssemblyInfo.cs
+++ CommBank-Server/obj/Debug/net6.0/CommBank.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommBank-Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5311469c7e03dbda08d11a26c479fce0218993e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommBank-Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommBank-Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
